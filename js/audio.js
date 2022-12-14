4db6fef7019aeba47f976cdc6fc8ac1914b874c5
alexdias777 <74875568+alexdias777@users.noreply.github.com>
2022-12-13 22:37:54 -0300

    Add files via upload

--- js/audio.js
+++ js/audio.js
@@ -1,21 +1,16 @@
 
-document.addEventListener("DOMContentLoaded", function(){
-    var audio = document.getElementById("audio");
-    
-    document.getElementById("play").addEventListener("click", function(){
-        audio.play();
-        console.log("Play");
-    }, false);
-    
-    document.getElementById("pause").addEventListener("click", function(){
-        audio.pause();
-        console.log("pause");
-    }, false);
-    
-    document.getElementById("volume").addEventListener("mousemove", function(){
-        var volume = document.getElementById("volume").value;
-        audio.volume = volume;
-        console.log(volume);
-    }, false);
-    
-}, false);			
+var myMusic= document.getElementById("mp3");
+
+function play() {
+
+myMusic.play();
+
+}
+
+ 
+
+function pause() {
+
+myMusic.pause();
+
+}
